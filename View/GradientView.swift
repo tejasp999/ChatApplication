b8b07d0677827f31040f1877554eba71ea228618
tejasp999 <spteja999@gmail.com>
2018-02-18 19:58:28 -0500

    SWRevealController and Login

---
+++ View/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  SmackApp
+//
+//  Created by Teja PV on 2/15/18.
+//  Copyright © 2018 Teja PV. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
