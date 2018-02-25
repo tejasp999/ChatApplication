96f39f4e75b6836f95e8c160d5e4a3a213245fc3
tejasp999 <spteja999@gmail.com>
2018-02-25 00:49:55 -0500

    Authentication Start

--- Utilities/Constants.swift
+++ Utilities/Constants.swift
@@ -7,5 +7,14 @@
 //
 
 import Foundation
+
+typealias CompletionHandler = (_ Success : Bool) -> ()
+let BASE_URL = "https://chatappslack.herokuapp.com/v1/"
+let Register_URL = "\(BASE_URL)account/register"
 let CREATE_ACCOUNT = "createAccount"
 let UN_WIND = "unwindToChannelVC"
+
+//User Defaults Keys
+let LOGGED_IN = "loggedIn"
+let TOKEN_KEY = "token"
+let USER_EMAIL = "userEmail"
