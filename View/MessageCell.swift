b39a65c465a035a2354a98b0979f4816d22757e3
tejasp999 <spteja999@gmail.com>
2018-06-06 20:20:04 -0500

    Date formatting

--- View/MessageCell.swift
+++ View/MessageCell.swift
@@ -24,6 +24,17 @@ class MessageCell: UITableViewCell {
         userNameLbl.text = message.userName
         userImage.image = UIImage (named: message.userAvatar)
         userImage.backgroundColor = UserService.instance.getUserColor(components: message.userAvatarColor)
+        guard var isoDate = message.timeStamp else { return }
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = isoDate.substring(to: end)
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm:a"
+        if let finaldate = chatDate{
+            let finaldate = newFormatter.string(from: finaldate)
+            timeStampLbl.text = finaldate
+        }
     }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
